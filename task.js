a959257e51e9c3a66e90b21ff4245f536d281b04
Aleksey Kirilenko <kirilenko.bsp@gmail.com>
2020-04-06 17:45:35 +0300

    ready homework 1.1 BJS-10

---
+++ task.js
@@ -0,0 +1,16 @@
+function averageMark(a, g, p) {
+	return (a + g + p) / 3;
+}
+
+function sayHello(userName) {
+	
+	return 'Привет, мир! Меня зовут ' + userName + '.';
+}
+
+function calculateFormula() {
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;
+    return result;
+}
